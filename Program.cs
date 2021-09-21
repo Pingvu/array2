e4838781b1b65f2948da4d2473e1880d0a98b83d
Johanness15 <jost04004@utb.vaxjo.se>
2021-09-21 14:28:12 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] temperatur={4.5, 3.2, 6.7, 6.1, 2.1, 1.6, 2.9};
+            double summa= 0;
+            foreach (double e in temperatur)
+            {
+                summa += e;
+            }
+            double result=(summa/temperatur.Length);
+            Console.WriteLine(Math.Round(result,2));
+        }
+    }
+}
